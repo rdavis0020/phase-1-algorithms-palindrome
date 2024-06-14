ab55136e1c8f0ad375598f3fd07a91ff9ae631f1
Ryan Davis <rdavis0020@gmail.com>
2024-06-14 13:10:00 -0400

    message complete

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+  function isPalindrome(s) {
+    // Compare the string to its reverse
+    return s === s.split('').reverse().join('');
 }
+
+// Examples:
+console.log(isPalindrome("mom"));      // Should return true
+console.log(isPalindrome("racecar"));  // Should return true
+console.log(isPalindrome("hello"));    // Should return false
+console.log(isPalindrome("noon"));     // Should return true
+
 
 /* 
   Add your pseudocode here
@@ -8,7 +16,7 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
-*/
+// *//Splits the string into an array of characters, reverses it, and then combines them back into a string.
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
